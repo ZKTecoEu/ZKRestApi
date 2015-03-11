34e0e12c1facdb2ce4fa44f7de4d7b4b5ba8d25f
gmerinojimenez <gmerinojimenez@gmail.com>
2015-03-11 11:00:07 +0100

    Small formatting
    Signed-off-by: guillermo <william.merino@zktechnology.eu>

--- ZKRestServer/app/filters/CorsFilter.scala
+++ ZKRestServer/app/filters/CorsFilter.scala
@@ -16,7 +16,7 @@ object CorsFilter extends Filter {
     nextFilter(requestHeader).map { result =>
       result.withHeaders("Access-Control-Allow-Origin" -> "*",
         "Access-Control-Allow-Methods" -> "POST, GET, OPTIONS, PUT, DELETE",
-        "Access-Control-Allow-Headers" -> "x-requested-with,content-type,Cache-Control,Pragma,Date, Accept, Authorization"
+        "Access-Control-Allow-Headers" -> "x-requested-with,content-type,Cache-Control,Pragma,Date,Accept,Authorization"
       )
     }
   }
